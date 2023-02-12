4026adf38d28bb340bdf1872ea93cd334530bbbe
Atish <atish202@gmail.com>
2023-02-12 17:51:30 +0530

    demo

---
+++ backend.tf
@@ -0,0 +1,10 @@
+terraform {
+  backend "s3" {
+    bucket = "mydev-tf-state-bucket-51"
+    key = "terraform.tfstate"
+    region = "ap-south-1"
+    dynamodb_table = "my-dynomo-table"
+
+    
+  }
+}
